6fd3c9bd1e8b70df73ede66e36e25b2569469e2f
kreenox <jeangatien.cordier@gmail.com>
2016-08-04 11:27:53 +0200

    ajout d'une fonction qui génère une boudary box pour les polygones 2d

--- code/core/2d.js
+++ code/core/2d.js
@@ -13,3 +13,23 @@ function point2D(x, y){
 		return false;
 	};
 }
+
+//retourne un polygone rectengulaire qui contient tout les points du polygone passé en parametre
+function buildABoundary(poly){
+	var Max = new Point2D(poly[0].x, poly[0].y);
+	var min = new Point2D(poly[0].x, poly[0].y);
+	
+	for(let n = 0; n < poly.length; n++)
+	{
+		if(max.x < poly[n].x)
+			max.x = poly[n].x;
+		if(max.y < poly[n].y)
+			max.y = poly[n].y;
+		if(min.x > poly[n].x)
+			min.x = poly[n].x;
+		if(min.y > poly[n].y)
+			min.y = poly[n].y;
+	}
+	
+	return [new Point2D(min.x, min.y), new Point2D(min.x, max.y), new Point2D(max.x, max.y), new Point2D(max.x, min.y)];
+}
